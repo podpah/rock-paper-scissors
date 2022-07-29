ff93b182180ed0039f74e6a605a5b14b56900841
podpah <pedrinhohrodrigues@outlook.com>
2022-07-29 15:32:54 +0100

    Commit

--- Multiverse_project/script.js
+++
@@ -1,9 +0,0 @@
-let rps = ["Rock", "Paper", "Scissors"]; let usc = 0; let bot = 0;
-
-bat = (str) => {
-    let rand = rps[Math.floor(Math.random() * rps.length)];
-    while (rand == str) {rand = rps[Math.floor(Math.random() * rps.length)]}; console.log(rand)
-    if (rand == "Rock") {if (str == "Paper") {usc++;out.value="Paper wraps rock, you've won"} else {bot++;out.value="Rock breaks scissors, you've lost"}}
-    else if (rand == "Paper") {if (str == "Scissors") {usc++;out.value="Scissors cut paper, you've won"} else {bot++;out.value="Paper wraps rock, you've lost"}}
-    else {if (str == "Rock") {usc++;out.value="Rock breaks scissors, you've won"} else {bot++;out.value="Scissors cut paper, you've lost"}}
-    rob.innerHTML = bot; use.innerHTML = usc;}
